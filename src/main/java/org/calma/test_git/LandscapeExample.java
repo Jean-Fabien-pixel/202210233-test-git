8f191ff6c1847ac2add0e0d8c4572f6bb7072557
jean-fabien-pixel <202210233@collegealma.ca>
2025-01-23 10:24:51 -0500

    third

--- src/main/java/org/calma/test_git/LandscapeExample.java
+++ src/main/java/org/calma/test_git/LandscapeExample.java
@@ -12,7 +12,7 @@ public class LandscapeExample extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("JavaFX Landscape");
+        primaryStage.setTitle("JavaFX Landscape with Camel");
 
         StackPane root = new StackPane();
         Canvas canvas = new Canvas(800, 600);
@@ -32,9 +32,34 @@ public class LandscapeExample extends Application {
         gc.setFill(Color.GREEN);
         gc.fillRect(0, 300, 800, 300);
 
+        // Draw the camel
+        drawCamel(gc, 400, 400);
+
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void drawCamel(GraphicsContext gc, double x, double y) {
+        gc.setFill(Color.BROWN);
+
+        // Body
+        gc.fillOval(x - 50, y - 50, 100, 50);
+
+        // Head
+        gc.fillOval(x + 40, y - 60, 40, 30);
+
+        // Neck
+        gc.fillOval(x + 20, y - 70, 30, 50);
+
+        // Legs
+        gc.fillOval(x - 30, y - 10, 20, 50);
+        gc.fillOval(x + 10, y - 10, 20, 50);
+//        gc.fillOval(x - 30, y + 30, 20, 50);
+//        gc.fillOval(x + 10, y + 30, 20, 50);
+
+        // Hump
+        gc.fillOval(x - 10, y - 70, 50, 40);
     }
 
     public static void main(String[] args) {

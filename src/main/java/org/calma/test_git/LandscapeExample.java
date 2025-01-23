01e24d0ab913761431987650bc69e0b7d45fbb8f
jean-fabien-pixel <202210233@collegealma.ca>
2025-01-23 10:26:13 -0500

    third

--- src/main/java/org/calma/test_git/LandscapeExample.java
+++ src/main/java/org/calma/test_git/LandscapeExample.java
@@ -33,33 +33,33 @@ public class LandscapeExample extends Application {
         gc.fillRect(0, 300, 800, 300);
 
         // Draw the camel
-        drawCamel(gc, 400, 400);
+        drawCamel(gc);
 
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private void drawCamel(GraphicsContext gc, double x, double y) {
+    private void drawCamel(GraphicsContext gc) {
         gc.setFill(Color.BROWN);
 
         // Body
-        gc.fillOval(x - 50, y - 50, 100, 50);
+        gc.fillOval((double) 400 - 50, (double) 400 - 50, 100, 50);
 
         // Head
-        gc.fillOval(x + 40, y - 60, 40, 30);
+        gc.fillOval((double) 400 + 40, (double) 400 - 60, 40, 30);
 
         // Neck
-        gc.fillOval(x + 20, y - 70, 30, 50);
+        gc.fillOval((double) 400 + 20, (double) 400 - 70, 30, 50);
 
         // Legs
-        gc.fillOval(x - 30, y - 10, 20, 50);
-        gc.fillOval(x + 10, y - 10, 20, 50);
+        gc.fillOval((double) 400 - 30, (double) 400 - 10, 20, 50);
+        gc.fillOval((double) 400 + 10, (double) 400 - 10, 20, 50);
 //        gc.fillOval(x - 30, y + 30, 20, 50);
 //        gc.fillOval(x + 10, y + 30, 20, 50);
 
         // Hump
-        gc.fillOval(x - 10, y - 70, 50, 40);
+        gc.fillOval((double) 400 - 10, (double) 400 - 70, 50, 40);
     }
 
     public static void main(String[] args) {
